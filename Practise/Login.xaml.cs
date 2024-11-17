fd4aca8ba67e542698abf83c1e13faffc3b846ff
omarfouad2024 <omar.mohamed.0523094@gmail.com>
2024-11-17 21:08:20 +0200

    firstcommit

---
+++ Practise/Login.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practise
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Signup_click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Signup());
+        }
+
+        private void Login_click(object sender, RoutedEventArgs e)
+        {
+            string name = Emailtext.Text;
+            string password = passtext.Text;  
+            if(name =="1" && password == "1")
+            {
+                NavigationService.Navigate(new Admin());
+                MessageBox.Show("welcome Succefully ");
+                return;
+            }
+
+             
+
+
+
+
+
+
+        }
+    }
+}
